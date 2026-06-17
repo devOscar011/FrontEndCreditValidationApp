f5cdf038562c7c129dfbd7ffdbe89e00e88ba02d
Marlon-Urtecho <urtechoalex@gmail.com>
2026-06-17 14:57:41 -0600

    produccion end2

--- Services/Api.js
+++ Services/Api.js
@@ -2,7 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import axios from 'axios'
 import Constants from 'expo-constants'
 
-const API_URL = Constants.expoConfig?.extra?.apiUrl || 'http://127.0.0.1:8000/api'
+const appConfig = Constants.expoConfig || Constants.manifest
+const API_URL = appConfig?.extra?.apiUrl || 'http://127.0.0.1:8000/api'
 
 const api = axios.create({
   baseURL: API_URL,
